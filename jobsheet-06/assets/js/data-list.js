88851183c40f898e24c6b3b65da4b1c711cfdad5
reismaa <apriliandv07@gmail.com>
2026-09-23 22:57:31 +0700

    8.4 Latihan no 3

--- jobsheet-06/assets/js/data-list.js
+++ jobsheet-06/assets/js/data-list.js
@@ -8,7 +8,7 @@ async function muatDaftarData(urlJson, daftarKunci) {
  
     try {
         // simulasi delay jaringan agar loading indicator terlihat
-        await new Promise((resolve) => setTimeout(resolve, 600));
+        await new Promise((resolve) => setTimeout(resolve, 3000));
  
         const res = await fetch(urlJson);
         if (!res.ok) {
